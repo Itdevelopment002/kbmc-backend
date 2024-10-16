4ed07bfa1a7d2419237d641309de71b3afa559c5
pragatitripathi24 <pragati.tripathi2401@gmail.com>
2024-10-16 12:46:46 +0530

    main menu

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -1,22 +1,28 @@
 import React from 'react';
-
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from '../Sidebar/Sidebar';
 import MainMenuTable from '../MainMenuTable/MainMenuTable';
-
 import Header from '../Header/Header';
+import AddMainPage from '../MainMenuTable/AddMainPage';
+
 
 const Dashboard = () => {
   return (
-    <div className="d-flex">
-     <Sidebar />
-      <div className="flex-grow-1">
-      <Header />
-        <div className="p-4">
-          <MainMenuTable />
+    <Router>
+      <div className="d-flex">
+        <Sidebar />
+        <div className="flex-grow-1">
+          <Header />
+          <div className="p-4">
+            {/* Define the routes here */}
+            <Routes>
+              <Route path="/" element={<MainMenuTable />} />
+              <Route path="/add-main" element={<AddMainPage />} />
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
